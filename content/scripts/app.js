93b42ee33354ab2bb6571c9f32bb0510bbec3cf9
omid <Omidfoladvand0919@gmail.com>
2025-01-31 20:37:47 +0330

    added category page

--- content/scripts/app.js
+++ content/scripts/app.js
@@ -1,1 +1,17 @@
 
+const categories = document.querySelectorAll('.category');
+
+categories.forEach(category => {
+    category.addEventListener('click', () => {
+        const categoryName = category.getAttribute('data-category');
+        
+        const params = new URLSearchParams();
+        params.append('category', categoryName);
+        
+        
+        const newUrl = `./content/pages/category.html?${params.toString()}`;
+        
+        window.location.href = newUrl;
+    });
+});
+
---
+++ content/scripts/category.js
@@ -0,0 +1,59 @@
+let categoryInformation = [
+    {id: 0 , name : "Pizza"  ,image :'../../images/category-img-1.jpg', Branches : ['Qom   Arak  Karaj Bandar Abbas'] , description : 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'},
+    {id: 1 , name : "Kebab"  ,image :'../../images/category-img-2.jpg', Branches : ['Shiraz   Lorestan  Isfahan '] , description : 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'},
+    {id: 2 , name : "Soup"  ,image :'../../images/category-img-3.jpg', Branches : ['Tehran  Isfahan  Arak   Karaj '] , description : 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'},
+    {id: 3 , name : "Sandwich"  ,image :'../../images/category-img-4.jpg', Branches : ['Bushehr  	Kuhsar  Bandar Abbas   Arak  Karaj Tabriz'] , description : 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'},
+    {id: 4 , name : "PersianCosine"  ,image :'../../images/category-img-5.jpg', Branches : ['Isfahan   Kuhsar   '] , description : 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'},
+    {id: 5 , name : "Fried"  ,image :'../../images/category-img-6.jpg', Branches : ['	Kerman  Kermanshah 	Ahvaz Sanandaj'] , description : 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'},
+    {id: 6 , name : "Pasta"  ,image :'../../images/category-img-7.jpg', Branches : ['Tabriz Sanandaj   Kuhsar   karaj'] , description : 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'},
+    {id: 7 , name : "Salad"  ,image :'../../images/category-img-8.jpg', Branches : ['Khorramabad  Rasht   	Azna   Babol'] , description : 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'},
+    {id: 8 , name : "Breakfast"  ,image :'../../images/category-img-9.jpg', Branches : [' Isfahan Qom   Rasht    Tabriz Bandar Abbas'] , description : 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'},
+    {id: 9 , name : "Steak"  ,image :'../../images/category-img-10.jpg', Branches : ['	Tabriz  Kuhsar Rasht   Babol   Nowshahr  Bojnord Qazvin Mashhad '] , description : 'Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.'},
+
+]
+
+
+const categoryImage = document.querySelector('.category-image')
+const statsContainer = document.querySelector('.Branches')
+
+
+
+
+function getCategoryFromURL() {
+    const params = new URLSearchParams(window.location.search);
+    const category = params.get('category'); 
+
+    return category;
+}
+
+function getCategoryInfo(categoryName) {
+    return categoryInformation.find(category => category.name.toLowerCase() === categoryName.toLowerCase());
+}
+
+const categoryName = getCategoryFromURL();
+
+if (categoryName) {
+    const categoryInfo = getCategoryInfo(categoryName);
+    
+    if (categoryInfo) {
+        console.log(categoryInfo);
+        document.getElementById('category-title').innerText = categoryInfo.name;
+        document.getElementById('category-description').innerText = categoryInfo.description;
+        categoryImage.setAttribute('src' , categoryInfo.image)
+        categoryInfo.Branches.forEach(Branch => {
+            const div = document.createElement('div')
+            div.innerHTML = Branch
+            statsContainer.appendChild(div)
+        })
+    } else {
+        console.log('Category not found');
+    }
+} else {
+    console.log('Category parameter not found in URL');
+}
+
+
+const backBtn = document.querySelector('#back-btn')
+backBtn.addEventListener('click' , () => {
+    window.history.back()
+})
